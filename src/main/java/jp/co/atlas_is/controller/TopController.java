f3bca0056914192047879ca85b83a019eebc2d20
shino <ms0127@hotmail.co.jp>
2019-01-12 13:47:03 +0900

    Merge branch '4thPractice' of https://github.com/atlasokayama/atlasbook.git
    Conflicts:
    	src/main/resources/templates/list.html

--- src/main/java/jp/co/atlas_is/controller/TopController.java
+++ src/main/java/jp/co/atlas_is/controller/TopController.java
@@ -1,11 +1,15 @@
 package jp.co.atlas_is.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import jp.co.atlas_is.dto.EmployeeInfoDto;
+import jp.co.atlas_is.form.EditForm;
 import jp.co.atlas_is.form.ListForm;
 
 @Controller
@@ -18,19 +22,7 @@ public class TopController {
 	 */
 	@RequestMapping(params = "list", method = RequestMethod.POST)
 	ModelAndView list() {
-		
-		// formを作成
-		ListForm form = new ListForm();
-		// 職員情報を作成
-		EmployeeInfoDto dto = new EmployeeInfoDto();
-		// 職員情報を格納
-		dto.setEmployeeNo("123456");
-		dto.setName("岡山　太郎");
-		// 職員情報をformに格納
-		form.setEmployeeInfo(dto);
-
-		// 遷移先情報を設定
-		ModelAndView mav = new ModelAndView("list", "form", form);
+		ModelAndView mav = new ModelAndView("list");
 		return mav;
 	}
 	
@@ -53,5 +45,40 @@ public class TopController {
 		ModelAndView mav = new ModelAndView("master");
 		return mav;
 	}
-	
+
+	/**
+	 * 演習用サンプル
+	 * @return モデル／ビュー
+	 */
+	@RequestMapping(params = "sample", method = RequestMethod.POST)
+	ModelAndView sample() {
+		// formを作成
+		ListForm form = new ListForm();
+		List<EditForm> list = new ArrayList<EditForm>();
+		EditForm edit = new EditForm();
+		
+		// 職員情報を作成
+		EmployeeInfoDto dto = new EmployeeInfoDto();
+		// 職員情報を格納
+		dto.setEmployeeNo("123456");
+		dto.setName("岡山　太郎");
+		// 職員情報をformに格納
+		edit.setEmployeeInfo(dto);
+		list.add(edit);
+
+		// 2件目
+		dto = new EmployeeInfoDto();
+		dto.setEmployeeNo("999999");
+		dto.setName("岡山　次郎");
+		edit = new EditForm();
+		edit.setEmployeeInfo(dto);		
+		list.add(edit);
+		
+		form.setAttendanceInfoList(list);
+
+		// 遷移先情報を設定
+		ModelAndView mav = new ModelAndView("sample", "form", form);
+		return mav;
+	}
+
 }
