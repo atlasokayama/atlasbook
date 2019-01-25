48dd184722de91966d6372c060e6f79cbcb5a1d0
Ryuichi Umeno <good-days_with_big-smile@hotmail.co.jp>
2019-01-25 11:58:51 +0900

    各種HTML/CSS整えました

--- src/main/java/jp/co/atlas_is/controller/MasterController.java
+++ src/main/java/jp/co/atlas_is/controller/MasterController.java
@@ -7,7 +7,18 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("master")
-public class MasterController {
+public class MasterController {	
+
+	/**
+	 * マスタ管理画面遷移処理
+	 * @return モデル／ビュー
+	 */
+	@RequestMapping(params = "master", method = RequestMethod.POST)
+	ModelAndView master() {
+		// 遷移先情報を設定
+		ModelAndView mav = new ModelAndView("master");
+		return mav;
+	}	
 	
 	/**
 	 * 出席者追加画面遷移処理
@@ -15,7 +26,6 @@ public class MasterController {
 	 */
 	@RequestMapping(params = "addEmp", method = RequestMethod.POST)
 	ModelAndView list() {
-		
 		// 遷移先情報を設定
 		ModelAndView mav = new ModelAndView("addEmp");
 		return mav;
