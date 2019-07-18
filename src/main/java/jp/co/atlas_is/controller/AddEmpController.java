78189cb1f5f6a4a7a2e2cc213bffffeaf4b9e913
Ryuichi Umeno <good-days_with_big-smile@hotmail.co.jp>
2019-07-19 06:18:52 +0900

    戻るボタンの追加

---
+++ src/main/java/jp/co/atlas_is/controller/AddEmpController.java
@@ -0,0 +1,9 @@
+package jp.co.atlas_is.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("addEmp")
+public class AddEmpController {
+}
