8311addbe44a41221ac6e94cdaee3c9ac0b0ed41
atlas-okayama <atlas-okayama@muraenidae>
2019-08-21 15:55:43 +0900

    DB接続演習用のサンプルソース追加

--- src/main/java/jp/co/atlas_is/controller/SampleController.java
+++ src/main/java/jp/co/atlas_is/controller/SampleController.java
@@ -1,5 +1,10 @@
 package jp.co.atlas_is.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
@@ -15,6 +20,7 @@ import jp.co.atlas_is.form.AttendanceInfoForm;
 import jp.co.atlas_is.form.EditForm;
 import jp.co.atlas_is.form.EmployeeInfoForm;
 import jp.co.atlas_is.form.ListForm;
+import jp.co.atlas_is.util.DbUtil;
 
 @Controller
 public class SampleController {
@@ -43,7 +49,7 @@ public class SampleController {
 	 * 
 	 * @return モデル／ビュー
 	 */
-	@RequestMapping(value = "/sample",params = "edit", method = RequestMethod.POST)
+	@RequestMapping(value = "/sample", params = "edit", method = RequestMethod.POST)
 	ModelAndView master(@Validated @ModelAttribute EditForm input, Errors errors) {
 		// エラーチェック
 		if (errors.hasErrors()) {
@@ -56,6 +62,41 @@ public class SampleController {
 		// 登録成功時は一覧画面へ遷移
 		// formを作成
 		ListForm form = new ListForm();
+		List<EditForm> list = new ArrayList<EditForm>();
+		
+		try {
+			// DBコネクションを取得
+			Connection con = DbUtil.getConnection();
+			
+			String sql = "select * from attendance a left join employee b on a.emp_no = b.emp_no;";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			
+			// SQLを実行
+			ResultSet rs = stmt.executeQuery();
+			
+			// 結果を取得
+			while (rs.next()) {
+				// 項目名指定で結果を取得してFormに格納
+				EditForm edit = new EditForm();
+				AttendanceInfoForm attend = new AttendanceInfoForm();
+				attend.setEmp_no(rs.getInt("emp_no"));
+				attend.setAm_attend(rs.getBoolean("am_attend"));
+				attend.setAm_reason(rs.getString("am_reason"));
+				attend.setPm_attend(rs.getBoolean("pm_attend"));
+				attend.setPm_reason(rs.getString("pm_reason"));
+				edit.setAttendanceInfo(attend);
+				EmployeeInfoForm emp = new EmployeeInfoForm();
+				emp.setName(rs.getString("emp_name"));
+				edit.setEmployeeInfo(emp);
+				list.add(edit);
+			}
+			// 取得結果を格納
+			form.setAttendanceInfoList(list);
+		} catch (Exception e) {
+			// エラー画面へ遷移
+			ModelAndView mav = new ModelAndView("error");
+			return mav;
+		}
 
 		// 遷移先情報を設定
 		ModelAndView mav = new ModelAndView("list", "form", form);
--- src/main/java/jp/co/atlas_is/util/DbUtil.java
+++ src/main/java/jp/co/atlas_is/util/DbUtil.java
@@ -1,21 +1,30 @@
 package jp.co.atlas_is.util;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 public class DbUtil {
 
-	public static Connection getConnection() {
+	public static Connection getConnection() throws SQLException, IOException {
+
+		// 設定ファイルの読み込み
+		Resource resource = new ClassPathResource("/application.properties");
+		Properties props = PropertiesLoaderUtils.loadProperties(resource);
+
+		// 接続情報を取得
+		String url = props.getProperty("db.url");
+		String user = props.getProperty("db.user");
+		String password = props.getProperty("db.password");
+
 		Connection con = null;
-		try {
-			con = DriverManager.getConnection("jdbc:postgresql://ec2-54-243-150-10.compute-1.amazonaws.com:5432/DB", 
-					"user", 
-					"pass");
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		
+		con = DriverManager.getConnection(url, user, password);
 		return con;
 	}
 }
