991bd85e277e3347f265eda47fd53a796d4cd6b7
shino <ms0127@hotmail.co.jp>
2020-02-08 15:55:30 +0900

    一覧画面の年月をForm値から取得するよう修正

--- src/main/java/jp/co/atlas_is/form/BaseForm.java
+++ src/main/java/jp/co/atlas_is/form/BaseForm.java
@@ -1,10 +1,20 @@
 package jp.co.atlas_is.form;
 
 import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 
 import lombok.Data;
 
 @Data
 public class BaseForm {
 	private YearMonth targetMonth;
+
+	@SuppressWarnings("unused")
+	private String dispYearMonth;
+
+	public String getDispYearMonth() {
+		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy年MM月");
+		return targetMonth.format(dtf1);
+	}
+	
 }
